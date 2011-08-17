9a7a4e9d641062f5242061b3062f1d6f1fc0b878
bnoguchi <brian.noguchi@gmail.com>
2011-08-16 22:11:29 -0700

    Move from subdomain to paths for examples.

--- server.js
+++ server.js
@@ -1,35 +1,27 @@
-var httpProxy = require('http-proxy')
-  , http = require('http')
-  , connect = require('connect')
+var connect = require('connect')
   , letters = require('racer/examples/letters/index')
   , todos = require('racer/examples/todos/index');
 
-httpProxy.createServer({
-    hostnameOnly: true
-  , router: {
-        'letters.racerjs.com': '127.0.0.1:3001'
-      , 'todos.racerjs.com': '127.0.0.1:3002'
-      , 'racerjs.com': '127.0.0.1: 3003'
-      , 'www.racerjs.com': '127.0.0.1:3003'
-    }
-}).listen(80);
+var app = connect(
+    connect.router( function (app) {
+      app.get('/', function (req, res, next) {
+        res.writeHead(303, { Location: 'https://github.com/codeparty/racer' });
+        return res.end();
+      });
+    })
+  , letters.app
+  , todos.app
+);
 
-connect(
-  connect.router( function (app) {
-    app.get('/', function (req, res, next) {
-      res.writeHead(303, { Location: 'https://github.com/codeparty/racer' });
-      return res.end();
-    });
-  })
-).listen(3003);
+var numApps = 2;
 
 // Clear any existing data, then initialize
 letters.store.flush( function (err) {
   if (err) throw err;
-  letters.app.listen(3001);
+  --numApps || app.listen(3000);
 });
 
 todos.store.flush( function (err) {
   if (err) throw err;
-  todos.app.listen(3002);
+  --numApps || app.listen(3000);
 });
