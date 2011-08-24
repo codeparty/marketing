64581d5c408e1920490f3d8d39c56b7dd29d7987
nateps <nate@nateps.com>
2011-08-24 00:57:34 -0700

    Fix bugs

--- server.js
+++ server.js
@@ -8,25 +8,25 @@ var proxyServer = httpProxy.createServer({
   router: {
     'racerjs.com/letters': '127.0.0.1:3001',
     'racerjs.com/todos': '127.0.0.1:3002',
-    'racerjs.com': '127.0.0.1:8001',
     'derbyjs.com/chat': '127.0.0.1:3003',
+    'racerjs.com': '127.0.0.1:8001',
     'derbyjs.com': '127.0.0.1:8002'
   }
 });
-proxyServer.listen(80);
+proxyServer.listen(8009);
 
-express.createServer()
-  .get('/', function (req, res) {
-    res.redirect('https://github.com/codeparty/racer');
-  })
-  .listen(8001);
+racerJs = express.createServer();
+racerJs.get('/', function (req, res) {
+  res.redirect('https://github.com/codeparty/racer');
+});
+racerJs.listen(8001);
 
-express.createServer()
-  .get('/', function (req, res) {
-    res.redirect('https://github.com/codeparty/derby');
-  })
-  .get('/robots.txt', (req, res) {
-    res.send('User-agent: *\nDisallow: /chat/', 'Content-Type': 'text/plain');
-  })
-  .listen(8002);
+derbyJs = express.createServer();
+derbyJs.get('/', function (req, res) {
+  res.redirect('https://github.com/codeparty/derby');
+});
+derbyJs.get('/robots.txt', function (req, res) {
+  res.send('User-agent: *\nDisallow: /chat/', {'Content-Type': 'text/plain'});
+});
+derbyJs.listen(8002);
 
