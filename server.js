517a6f08e3cb209642d30e428ecf942e3ed02d92
nateps <nate@nateps.com>
2011-08-24 00:31:02 -0700

    rewrite to use a proxy

--- server.js
+++ server.js
@@ -1,35 +1,32 @@
-var connect = require('connect')
-  , letters = require('racer/examples/letters/index')
-  , todos = require('racer/examples/todos/index')
-  , racer = require('racer')
-  , socketio = require('socket.io');
+var httpProxy = require('http-proxy')
+  , express = require('express')
+  , letters = require('racer/examples/letters')
+  , todos = require('racer/examples/todos')
+  , chat = require('derby/examples/chat');
 
-var app = connect(
-    connect.router( function (app) {
-      app.get('/', function (req, res, next) {
-        res.writeHead(303, { Location: 'https://github.com/codeparty/racer' });
-        return res.end();
-      });
-    })
-);
+var proxyServer = httpProxy.createServer({
+  router: {
+    'racerjs.com/letters': '127.0.0.1:3001',
+    'racerjs.com/todos': '127.0.0.1:3002',
+    'racerjs.com': '127.0.0.1:8001',
+    'derbyjs.com/chat': '127.0.0.1:3003',
+    'derbyjs.com': '127.0.0.1:8002'
+  }
+});
+proxyServer.listen(80);
 
-var io = socketio.listen(app);
+express.createServer()
+  .get('/', function (req, res) {
+    res.redirect('https://github.com/codeparty/racer');
+  })
+  .listen(8001);
 
-app.use(
-  letters(racer.createStore({
-      redis: {db: 1}
-    , sockets: io.of('/sio/letters')
-    , socketUri: '/sio/letters'
-  })).app
-);
-
-app.use(
-  todos(racer.createStore({
-      redis: {db: 2}
-    , sockets: io.of('/sio/todos')
-    , socketUri: '/sio/todos'
-  })).app
-);
-
-app.listen(process.env.NODE_ENV == 'production' ? 80 : 3000);
+express.createServer()
+  .get('/', function (req, res) {
+    res.redirect('https://github.com/codeparty/derby');
+  })
+  .get('/robots.txt', (req, res) {
+    res.send('User-agent: *\nDisallow: /chat/', 'Content-Type': 'text/plain');
+  })
+  .listen(8002);
 
