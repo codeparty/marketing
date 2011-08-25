356af579cd15a94745c748551e335e906911d42e
nateps <nate@nateps.com>
2011-08-24 22:04:01 -0700

    Write custom proxy

--- server.js
+++ server.js
@@ -4,19 +4,43 @@ var httpProxy = require('http-proxy')
   , todos = require('racer/examples/todos')
   , chat = require('derby/examples/chat');
 
-var proxyServer = httpProxy.createServer({
-  router: {
-    'racerjs.com/letters/': '127.0.0.1:3001',
-    'racerjs.com/todos/': '127.0.0.1:3002',
-    'derbyjs.com/chat/': '127.0.0.1:3003',
-    'racerjs.com': '127.0.0.1:8001',
-    'derbyjs.com': '127.0.0.1:8002'
+var proxyServer = httpProxy.createServer(function (req, res, proxy) {
+  var host = req.headers.host,
+      url = req.url,
+      reLetters = /^\/letters\//,
+      iLetters = reLetters.source.length,
+      reTodos = /^\/todos\//,
+      iTodos = reTodos.source.length,
+      reChat = /^\/chat\//,
+      iChat = reChat.source.length;
+  
+  if (host === 'racerjs.com') {          
+    if (reLetters.test(url)) {
+      req.url = req.url.substr(iLetters);
+      proxy.proxyRequest(req, res, { host: 'localhost', port: '3001' });
+    } else if (reTodos.test(url)) {
+      req.url = req.url.substr(iTodos);
+      proxy.proxyRequest(req, res, { host: 'localhost', port: '3002' });
+    } else {
+      proxy.proxyRequest(req, res, { host: 'localhost', port: '8001' });
+    }
+  
+  } else if (host === 'derbyjs.com') {
+    if (reChat.test(url)) {
+      req.url = req.url.substr(iChat);
+      proxy.proxyRequest(req, res, { host: 'localhost', port: '3003' });
+    } else {
+      proxy.proxyRequest(req, res, { host: 'localhost', port: '8002' });
+    }
   }
-});
-proxyServer.listen(process.env.NODE_ENV == 'production' ? 80 : 8080);
+
+}).listen(process.env.NODE_ENV == 'production' ? 80 : 8080);
+
 
 racerJs = express.createServer();
 racerJs.get('/', function (req, res) {
+  var host = req.headers.host,
+      url = req.
   res.redirect('https://github.com/codeparty/racer');
 });
 racerJs.listen(8001);
