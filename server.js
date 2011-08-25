5c115299bfbc02bb5fb539a0543b91742ccd7e27
nateps <nate@nateps.com>
2011-08-24 22:49:43 -0700

    Ugh, switching back to hostnames

--- server.js
+++ server.js
@@ -4,45 +4,20 @@ var httpProxy = require('http-proxy')
   , todos = require('racer/examples/todos')
   , chat = require('derby/examples/chat');
 
-var proxyServer = httpProxy.createServer(function (req, res, proxy) {
-  var host = req.headers.host,
-      url = req.url,
-      reIo = /\/socket.io\//,
-      reLetters = /^\/letters\//,
-      iLetters = reLetters.source.length - 4,
-      reTodos = /^\/todos\//,
-      iTodos = reTodos.source.length - 4,
-      reChat = /^\/chat\//,
-      iChat = reChat.source.length - 4;
-  
-  if (host === 'racerjs.com') {          
-    if (reLetters.test(url)) {
-      if (!reIo.test(url)) req.url = req.url.substr(iLetters);
-      console.log(req.url);
-      proxy.proxyRequest(req, res, { host: 'localhost', port: '3001' });
-    } else if (reTodos.test(url)) {
-      if (!reIo.test(url)) req.url = req.url.substr(iTodos);
-      proxy.proxyRequest(req, res, { host: 'localhost', port: '3002' });
-    } else {
-      proxy.proxyRequest(req, res, { host: 'localhost', port: '8001' });
-    }
-  
-  } else if (host === 'derbyjs.com') {
-    if (reChat.test(url)) {
-      if (!reIo.test(url)) req.url = req.url.substr(iChat);
-      proxy.proxyRequest(req, res, { host: 'localhost', port: '3003' });
-    } else {
-      proxy.proxyRequest(req, res, { host: 'localhost', port: '8002' });
-    }
+httpProxy.createServer({
+  hostnameOnly: true,
+  router: {
+    'letters.racerjs.com': '127.0.0.1:3001',
+    'todos.racerjs.com': '127.0.0.1:3002',
+    'racerjs.com': '127.0.0.1:8001',
+    'chat.derbyjs.com': '127.0.0.1:3003',
+    'derbyjs.com': '127.0.0.1:8002'
   }
-
 }).listen(process.env.NODE_ENV == 'production' ? 80 : 8080);
 
 
 racerJs = express.createServer();
 racerJs.get('/', function (req, res) {
-  var host = req.headers.host,
-      url = req.
   res.redirect('https://github.com/codeparty/racer');
 });
 racerJs.listen(8001);
@@ -50,9 +25,6 @@ racerJs.listen(8001);
 derbyJs = express.createServer();
 derbyJs.get('/', function (req, res) {
   res.redirect('https://github.com/codeparty/derby');
-});
-derbyJs.get('/robots.txt', function (req, res) {
-  res.send('User-agent: *\nDisallow: /chat/', {'Content-Type': 'text/plain'});
 });
 derbyJs.listen(8002);
 
