23d8dca985ba6dc882932eb1c47524cfbfe77cf6
nateps <nate@nateps.com>
2011-10-31 12:06:43 -0700

    update required versions

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
 # Code Party marketing site
 
-This is the marketing site for Code Party projects. Currently it hosts the demos for Derby and Racer.
-
-To get up and running on your local machine:
-
-1. Npm link racer & derby
-2. Npm install the rest of the modules in package.json
-3. Edit /etc/hosts to include racerjs.com, derbyjs.com
+This is the marketing site for Code Party projects. Currently it hosts the demos for [Derby](http://derbyjs.com/) and [Racer](http://racerjs.com/).
--- server.js
+++ server.js
@@ -1,20 +1,11 @@
-var httpProxy = require('http-proxy')
+var bouncy = require('bouncy')
   , express = require('express')
   , letters = require('racer/examples/letters')
+  , pad = require('racer/examples/pad')
   , todos = require('racer/examples/todos')
+  , hello = require('derby/examples/hello')
+  , sink = require('derby/examples/sink')
   , chat = require('derby/examples/chat');
-
-httpProxy.createServer({
-  hostnameOnly: true,
-  router: {
-    'letters.racerjs.com': '127.0.0.1:3001',
-    'todos.racerjs.com': '127.0.0.1:3002',
-    'racerjs.com': '127.0.0.1:8001',
-    'chat.derbyjs.com': '127.0.0.1:3003',
-    'derbyjs.com': '127.0.0.1:8002'
-  }
-}).listen(process.env.NODE_ENV == 'production' ? 80 : 8080);
-
 
 racerJs = express.createServer();
 racerJs.get('/', function (req, res) {
@@ -22,9 +13,22 @@ racerJs.get('/', function (req, res) {
 });
 racerJs.listen(8001);
 
-derbyJs = express.createServer();
-derbyJs.get('/', function (req, res) {
-  res.redirect('https://github.com/codeparty/derby');
-});
-derbyJs.listen(8002);
-
+bouncy(function (req, bounce) {
+  switch (req.headers.host) {
+    case 'racerjs.com':
+    case 'www.racerjs.com':
+      return bounce(8001);
+    case 'letters.racerjs.com':
+      return bounce(3010);
+    case 'pad.racerjs.com':
+      return bounce(3011);
+    case 'todos.racerjs.com':
+      return bounce(3012);
+    case 'hello.derbyjs.com':
+      return bounce(3000);
+    case 'sink.derbyjs.com':
+      return bounce(3001);
+    case 'chat.derbyjs.com':
+      return bounce(3002);
+  }
+}).listen(process.env.NODE_ENV == 'production' ? 80 : 8080);
